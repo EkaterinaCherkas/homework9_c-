47d6a6072965cd394eada9f62a8832afa7ecd1db
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-31 09:48:25 +0300

    add task2 рекурсия,находит сумму натур эл от M до N

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+using System;
+using static System.Console;
+Clear();
+WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+WriteLine("Введите начальное число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+void FindSumNumber (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    FindSumNumber(numberM, numberN, sum);
+}
+
+FindSumNumber(numberM, numberN, 0);
