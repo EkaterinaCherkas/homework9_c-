d1ad11b828a1ffce66242a1ee9b39ba86c6f2663
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-31 09:47:08 +0300

    add task1 рекурсия,выводит натур. числа от N  до 1

---
+++ task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using System;
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+for(int i = 1; i<=number;i++){
+Write($"{i} " );
+}
+WriteLine();
+int count = 1;
+NaturalNum(number, count);
+
+void NaturalNum(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        NaturalNum(n, count + 1);
+        Write(count + " ");
+    }
+}
